9f150e9b1df087c2ed73b97f882d17166851313f
Marcus.A.Karlsson <Marcus.A.Karlsson@cybercom.com>
2017-03-13 08:06:25 +0100

    Second commit
    Added map and functionality to add a marker

--- app/src/main/java/se/devner/gpsa/MainActivity.java
+++ app/src/main/java/se/devner/gpsa/MainActivity.java
@@ -1,14 +1,109 @@
 package se.devner.gpsa;
 
+import android.location.Location;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.FloatMath;
+import android.widget.SeekBar;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    GoogleMap map;
+    MarkerOptions clickedMarker;
+    boolean alarmActive;
+    float selectedRange;
+    SeekBar sb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //TESTeeeehjhjh
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        clickedMarker = null;
+        alarmActive = false;
+        selectedRange = 0f;
+        sb = (SeekBar) findViewById(R.id.seekBar2);
+        sb.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                selectedRange = (float) progress;
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+    @Override
+    public void onMapReady(GoogleMap m) {
+        map = m;
+
+        // Setting a click event handler for the map
+        map.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+
+            @Override
+            public void onMapClick(LatLng latLng) {
+
+                // Creating a marker
+                MarkerOptions markerOptions = new MarkerOptions();
+
+                // Setting the position for the marker
+                markerOptions.position(latLng);
+
+                // Setting the title for the marker.
+                // This will be displayed on taping the marker
+                markerOptions.title(latLng.latitude + " : " + latLng.longitude);
+
+                // Clears the previously touched position
+                map.clear();
+
+                // Animating to the touched position
+                map.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+
+                // Placing a marker on the touched position
+                map.addMarker(markerOptions);
+                clickedMarker = markerOptions;
+            }
+        });
+    }
+
+    public boolean userWithinRange(MarkerOptions m1, MarkerOptions m2){
+        if(meterDistanceBetweenPoints((float)m1.getPosition().latitude, (float)m1.getPosition().longitude, (float)m2.getPosition().latitude, (float)m2.getPosition().longitude) < selectedRange){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    private double meterDistanceBetweenPoints(float lat_a, float lng_a, float lat_b, float lng_b) {
+        float pk = (float) (180.f/Math.PI);
+
+        float a1 = lat_a / pk;
+        float a2 = lng_a / pk;
+        float b1 = lat_b / pk;
+        float b2 = lng_b / pk;
+
+        float t1 = (float)(Math.cos(a1)*Math.cos(a2)*Math.cos(b1)*Math.cos(b2));
+        float t2 = (float)(Math.cos(a1)*Math.sin(a2)*Math.cos(b1)*Math.sin(b2));
+        float t3 = (float)(Math.sin(a1)*Math.sin(b1));
+        double tt = Math.acos(t1 + t2 + t3);
+
+        return 6366000*tt;
     }
 }
