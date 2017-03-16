20086bbffdbd2b73343b796e3a4156fac5bb524a
Marcus.A.Karlsson <Marcus.A.Karlsson@cybercom.com>
2017-03-16 12:37:15 +0100

    Extended functionality
    Not fully working but extended functionality is there. Still need to get
    the phones exact location, other  wise it works.

--- app/src/main/java/se/devner/gpsa/MainActivity.java
+++ app/src/main/java/se/devner/gpsa/MainActivity.java
@@ -1,26 +1,49 @@
 package se.devner.gpsa;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Criteria;
 import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.CountDownTimer;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.FloatMath;
+import android.util.Log;
+import android.view.View;
 import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, LocationListener {
 
     GoogleMap map;
     MarkerOptions clickedMarker;
+    LatLng currentLocation;
+    CountDownTimer cdt;
     boolean alarmActive;
-    float selectedRange;
+    boolean GPSActivated;
+    double selectedRange;
     SeekBar sb;
+    ToggleButton tb;
+    TextView tv;
+    Circle circle;
+    LocationManager service;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,14 +52,43 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         SupportMapFragment mapFragment =
                 (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        cdt = null;
+        GPSActivated = false;
+
+        tv = (TextView) findViewById(R.id.textView);
+        tb = (ToggleButton) findViewById(R.id.toggleButton2);
+        tb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (tb.isChecked()) {
+                    if (GPSActivated) {
+                        if (clickedMarker != null) {
+                            Toast.makeText(MainActivity.this, "Alarm activated", Toast.LENGTH_SHORT).show();
+                            startCheck();
+                        } else {
+                            tb.setChecked(false);
+                            Toast.makeText(MainActivity.this, "Please choose a place for your alarm", Toast.LENGTH_SHORT).show();
+                        }
+                    } else {
+                        checkGPS();
+                    }
+                } else {
+                    Toast.makeText(MainActivity.this, "Alarm deactivated", Toast.LENGTH_SHORT).show();
+                    stopCheck();
+                }
+            }
+        });
         clickedMarker = null;
+        circle = null;
+        currentLocation = null;
         alarmActive = false;
-        selectedRange = 0f;
+        selectedRange = 0;
         sb = (SeekBar) findViewById(R.id.seekBar2);
         sb.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                selectedRange = (float) progress;
+                selectedRange = (double) progress;
+                tv.setText(String.valueOf(progress) + " meters");
             }
 
             @Override
@@ -46,10 +98,77 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
-
+                if (clickedMarker != null) {
+                    drawCircle(clickedMarker, selectedRange);
+                }
             }
         });
+
+        checkGPS();
     }
+
+    public void checkGPS() {
+        String provider;
+
+        service = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        // Define the criteria how to select the locatioin provider -> use
+        // default
+        Criteria criteria = new Criteria();
+        provider = service.getBestProvider(criteria, false);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        Location location = service.getLastKnownLocation(provider);
+
+        // Initialize the location fields
+        if (location != null) {
+            onLocationChanged(location);
+            GPSActivated = true;
+        } else {
+            GPSActivated = false;
+        }
+
+        service = (LocationManager) getSystemService(LOCATION_SERVICE);
+        boolean enabled = service
+                .isProviderEnabled(LocationManager.GPS_PROVIDER);
+        if (!enabled) {
+            Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            startActivity(intent);
+        }else{
+        }
+    }
+
+    private void startCheck() {
+        new CountDownTimer(30000000, 2000) {
+
+            public void onTick(long millisUntilFinished) {
+                //currentLocation = new LatLng(service.getLastKnownLocation().getLatitude(), gpsTracker.getLongitude());
+                Log.d("currLoc:", currentLocation.latitude + " " + currentLocation.longitude);
+                if(currentLocation != null) {
+                    Toast.makeText(MainActivity.this, "LocationNotNull", Toast.LENGTH_SHORT).show();
+                    if (userWithinRange(clickedMarker, currentLocation)) {
+                        //Notify
+                        Toast.makeText(MainActivity.this, "WITHIN RANGE", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+            public void onFinish() {
+                stopCheck();
+            }
+        }.start();
+    }
+
+    private void stopCheck() {
+        cdt = null;
+    }
+
     @Override
     public void onMapReady(GoogleMap m) {
         map = m;
@@ -77,14 +196,29 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                 map.animateCamera(CameraUpdateFactory.newLatLng(latLng));
 
                 // Placing a marker on the touched position
-                map.addMarker(markerOptions);
                 clickedMarker = markerOptions;
+                drawCircle(clickedMarker, selectedRange);
+                map.addMarker(markerOptions);
             }
         });
     }
 
-    public boolean userWithinRange(MarkerOptions m1, MarkerOptions m2){
-        if(meterDistanceBetweenPoints((float)m1.getPosition().latitude, (float)m1.getPosition().longitude, (float)m2.getPosition().latitude, (float)m2.getPosition().longitude) < selectedRange){
+    private void drawCircle(MarkerOptions clickedMarker, double selectedRange) {
+
+        if(circle != null){
+            circle.remove();
+        }
+        circle = map.addCircle(new CircleOptions()
+            .center(new LatLng(clickedMarker.getPosition().latitude, clickedMarker.getPosition().longitude))
+            .radius(selectedRange/2)
+            .strokeColor(Color.argb(100,0,0,0))
+            .fillColor(Color.argb(100, 100, 150, 200)));
+    }
+
+    public boolean userWithinRange(MarkerOptions sel, LatLng cur){
+        Log.d("SelectedLocation:", String.valueOf(sel.getPosition().latitude) + String.valueOf(sel.getPosition().longitude));
+        Log.d("CurrentLocation:", String.valueOf(cur.latitude) + String.valueOf(cur.longitude));
+        if(meterDistanceBetweenPoints((float)sel.getPosition().latitude, (float)sel.getPosition().longitude, (float)cur.latitude, (float)cur.longitude) < selectedRange){
             return true;
         }else{
             return false;
@@ -104,6 +238,28 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         float t3 = (float)(Math.sin(a1)*Math.sin(b1));
         double tt = Math.acos(t1 + t2 + t3);
 
+        Log.d("SelectedRange:", String.valueOf(selectedRange));
+        Log.d("Meters between:", String.valueOf(6366000*tt));
         return 6366000*tt;
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        currentLocation = new LatLng(location.getLatitude(), location.getLongitude());
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
     }
 }
